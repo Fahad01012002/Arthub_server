b46c593c1d6f4feed622aa6ba8cc6e92ee442a73
Fahad01012002 <fahad01012002@gmail.com>
2026-06-28 23:36:24 +0600

    add verifyToken middleware

--- index.js
+++ index.js
@@ -30,7 +30,46 @@ client.connect(() => {
     console.log('conecting to MongoDb');
 }).catch(console.dir)
 
+const database = client.db("art_hub_db");
+const paintingCardCollection = database.collection('paintingCard');
+const userCollection = database.collection('user');
+const planCollection = database.collection('plans');
+const transactionCollection = database.collection('transactions');
+const sessionCollection = database.collection('session');
 
+
+const verifyToken = async (req, res, next) => {
+
+    const headers = req.headers?.authorization;
+
+    if (!headers) {
+        return res.status(401).send({
+            messagte: 'Unauthorized access'
+        })
+    }
+
+    const token = headers.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).send({
+            message: 'Unauthorised Access'
+        })
+    }
+
+    const query = {
+        token: token
+    }
+    const session = await sessionCollection.findOne(query);
+    const userId = session.userId;
+
+    const userQuery = {
+        _id: userId
+    }
+
+    const user = await userCollection.findOne(userQuery);
+    req.user = user;
+    next();
+}
 
 
 
